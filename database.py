64b58a3ec07a2eae29cda37c19e36effe354578f
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2025-11-20 10:59:25 +0500

    Update database connection credentials

--- database.py
+++ database.py
@@ -1,19 +1,15 @@
 from sqlalchemy import (
-    create_engine, URL, MetaData
+    create_engine, URL, MetaData,
 )
 import config
 
 DATABASE_URL = URL.create(
     "postgresql+psycopg2",
     username=config.DB_USER,
-    password=config.DB_PASSWORD,
+    password=config.DB_PASS,
     host=config.DB_HOST,
     port=config.DB_PORT,
     database=config.DB_NAME,
 )
 engine = create_engine(DATABASE_URL)
-
 metadata_obj = MetaData()
-
-def get_connection():
-    return engine.connect()
